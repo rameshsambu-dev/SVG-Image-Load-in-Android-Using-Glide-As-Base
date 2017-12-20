7256bfb15b32777a1e9507e0efc326d54acc8d29
Ramesh Sambu <rameshsambu44@gmail.com>
2017-12-20 11:55:45 +0530

    second commit
    second commit

---
+++ app/src/main/java/fbfriendlistexample/com/demo/MainActivity.java
@@ -0,0 +1,32 @@
+package fbfriendlistexample.com.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.ahmadrosid.svgloader.SvgLoader;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView image;
+    private String url;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_item);
+        url = "http://www.clker.com/cliparts/u/Z/2/b/a/6/android-toy-h.svg";
+
+        image = (ImageView) findViewById(R.id.ivimage);
+
+        SvgLoader.pluck()
+                .with(this)
+                .setPlaceHolder(R.mipmap.ic_launcher, R.mipmap.ic_launcher)
+                .load(url, image);
+
+    }
+
+    @Override protected void onDestroy() {
+        super.onDestroy();
+        SvgLoader.pluck().close();
+    }
+}
---
+++ app/src/main/java/fbfriendlistexample/com/demo/MapView.java
@@ -0,0 +1,13 @@
+package fbfriendlistexample.com.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_view);
+    }
+}
